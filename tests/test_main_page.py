b8cefa9b424fa201c23f4b4b1eff200504400f4c
Chapuis <cyrille.chapuis@external.aldebaran.com>
2024-01-22 18:03:35 +0100

    report etude

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,9 +1,27 @@
-
 import pytest
+import requests
+from http import HTTPStatus
+from pytest_metadata.plugin import metadata_key
+
+from param import API_TOKEN
+
+
+
+def pytest_configure(config):
+    config._metadata['URL'] = os.environ['url']
+
 
 def test_main():
     assert True
 
 
-def test_main_2():
-    assert False
+@pytest.mark.parametrize("city", [["Poitiers",True], ["Paris",True],["Blargoug",False]])
+def test_main_2(city):
+    response = requests.get(
+        "http://api.openweathermap.org/data/2.5/weather?q="+city[0]+",fr&appid="
+        + API_TOKEN
+    )
+    if city[1]:
+        assert response.status_code == HTTPStatus.OK, "Return should be OK"
+    else:
+        assert response.status_code == HTTPStatus.NOT_FOUND, "Return should be not found"
